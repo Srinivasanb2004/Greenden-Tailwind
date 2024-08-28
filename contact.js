6899307d7d04326c54ad151176160191ef1e5007
srinivasanb2004 <seenusrinivas2004@gmail.com>
2024-08-28 13:28:35 +0530

    modified contact form

--- contact.js
+++ contact.js
@@ -2,12 +2,18 @@ var sidenav = document.getElementById("sidenav")
 var closenav = document.getElementById("closenav")
 var menuicon = document.getElementById("menuicon")
 
-menuicon.addEventListener("click",function(){
-    sidenav.style.right=0
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
 })
 
-closenav.addEventListener("click",function(){
-    sidenav.style.right="-50%"
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
 })
-    
+
+var btn = document.getElementById("btn").addEventListener("click", function () {
+    event.preventDefault()
+
+    alert("Thank you for visiting our page. Our team will reach out to you shortly!")
+})
+
 
